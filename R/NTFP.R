e375444171ee3dc1ef18fbb88e332590f5a5087f
JoeOToole73 <joseph.otoole73@gmail.com>
2025-10-02 15:18:19 +0100

    NTFP -> off farm: attempt 5 [ci skip]

--- R/NTFP.R
+++ R/NTFP.R
@@ -966,7 +966,12 @@ ntfp_totals <- function(tree_aid_df,
 
         off_farm_prop[off_farm_incomes_any=="n"] <- 0
 
-        indicator_df$off_farm_income_given_ntfp_lcu <- (off_farm_prop * (indicator_df$crop_income_lcu_per_year + indicator_df$livestock_income_lcu_per_year  + indicator_df$ntfp_income)) / (1 - off_farm_prop)
+        crop_inc_no_na = replace_na(data = indicator_df$crop_income_lcu_per_year, replace=0)
+        livestock_inc_no_na = replace_na(data = indicator_df$livestock_income_lcu_per_year, replace=0)
+        ntfp_inc_no_na = replace_na(data = indicator_df$ntfp_income, replace=0)
+
+
+        indicator_df$off_farm_income_given_ntfp_lcu <- (off_farm_prop * (crop_inc_no_na + livestock_inc_no_na  + ntfp_inc_no_na)) / (1 - off_farm_prop)
 
         return(indicator_df)
     }
